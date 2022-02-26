7b27d8b1415dcbec8a752fe9f28be5965a272d6b
jikiweng <94891522+jikiweng@users.noreply.github.com>
2022-02-26 14:50:14 +0900

    Add files via upload

---
+++ Combat/Weapon.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace RPG.Combat
+{
+    public class Weapon : MonoBehaviour
+    {
+        [SerializeField] UnityEvent onHit;
+
+        public void OnHit()
+        {
+            onHit.Invoke();
+        }
+    }
+}
